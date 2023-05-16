237db455cdc8251e77d594f2c93ba8207bae6545
Duc-Canh Nguyen <Duc-Canh.Nguyen@gipsa-lab.grenoble-inp.fr>
2023-05-16 20:50:24 +0200

    Update algos.h
    add bool customiser

--- sm/csm/algos.h
+++ sm/csm/algos.h
@@ -133,7 +133,7 @@ struct sm_params {
 	/* Parameter specific to HSM (unfinished :-/ ) */
 	struct hsm_params hsm;
 	
-	char *custom;
+	bool customiser;
 	int min_corresp_bef;
 	int min_corresp_aft;
 };
